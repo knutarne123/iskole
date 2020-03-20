8f5c66205863e5d1168c9257085440585ef0f2e7
Thomas Hannestad <thomashannestad@users.noreply.github.com>
2020-03-20 15:00:56 +0100

    Update sidebars.js

--- iskole/sidebars.js
+++ iskole/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     'Time- og fagfordeling': ['tf_klassefag'],
-    Timeplan: ['timeplan_blokknr', 'timeplan_klasse'],
+    Timeplan: ['timeplan_blokknr', 'timeplan_klasse', 'tp_kopiering'],
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
   },
