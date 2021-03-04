14283df80771864338b181e3946752b73072c032
Thomas Hannestad <thomashannestad@users.noreply.github.com>
2021-03-04 09:01:49 +0100

    Update index.js

--- iskole/src/pages/index.js
+++ iskole/src/pages/index.js
@@ -72,7 +72,7 @@ function Home() {
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/doc1')}>
-              Get Started
+              Gå til dokumentasjonen
             </Link>
           </div>
         </div>
