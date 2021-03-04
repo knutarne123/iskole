41f150955e7834de5f55705a2750430a54e9803d
Thomas Hannestad <thomashannestad@users.noreply.github.com>
2021-03-04 09:10:14 +0100

    Update index.js

--- iskole/src/pages/index.js
+++ iskole/src/pages/index.js
@@ -71,7 +71,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/doc1')}>
+              to={useBaseUrl('docs/oversikt')}>
               Gå til dokumentasjonen
             </Link>
           </div>
